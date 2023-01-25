8a93a42140b44ba1dde9edd903beafde7d7f7a4a
Robby Lewis <Robbysim37@gmail.com>
2023-01-25 18:32:40 -0500

    I hate bloomtech

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,23 +1,47 @@
 const router = require('express').Router()
+const dbFunctions = require(`./accounts-model`)
+const middleware = require("./accounts-middleware")
+
+const checkAccountId = middleware.checkAccountId
+const checkAccountNameUnique = middleware.checkAccountNameUnique
+const checkAccountPayload = middleware.checkAccountPayload
 
 router.get('/', (req, res, next) => {
   // DO YOUR MAGIC
+  dbFunctions.getAll().then( arr => {
+    res.status(200).send(arr)
+  })
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
+  res.status(200).send(req.foundAccount)
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', checkAccountNameUnique,checkAccountPayload, (req, res, next) => {
   // DO YOUR MAGIC
+  const validAccount = req.validAccount
+  dbFunctions.create(validAccount).then( arr => {
+    const createdAccountid = arr[0]
+    res.status(201).send({...validAccount,createdAccountid})
+  })
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', checkAccountId, checkAccountPayload, (req, res, next) => {
   // DO YOUR MAGIC
+  const id = req.params.id
+  const validAccount = req.validAccount
+  dbFunctions.updateById(id,validAccount).then( arr => {
+    res.status(200).send(req.body)
+  })
 });
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id',checkAccountId, (req, res, next) => {
   // DO YOUR MAGIC
+  const id = req.params.id
+  dbFunctions.deleteById(id).then(acc => {
+    res.status(200).send(req.foundAccount)
+  })
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
